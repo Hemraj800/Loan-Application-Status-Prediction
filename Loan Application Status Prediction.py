13d2858d35ed03a62a78772031f7e2bca474c510
Hemraj Jagtap <128118623+Hemraj800@users.noreply.github.com>
2023-09-17 17:09:28 +0530

    Add files via upload
    Loan Application Status Prediction

---
+++ Loan Application Status Prediction.py
@@ -0,0 +1,677 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[2]:
+
+
+data = pd.read_csv(r"C:\Users\HP\Downloads\loan_prediction.csv")
+
+
+# In[3]:
+
+
+data
+
+
+# In[4]:
+
+
+data.head()
+
+
+# In[5]:
+
+
+data.info()
+
+
+# In[6]:
+
+
+data.apply(lambda x: sum(x.isnull()),axis=0) # checking missing values in each column of train dataset
+
+
+# In[7]:
+
+
+data['Gender'].value_counts()
+
+
+# In[8]:
+
+
+data.Gender = data.Gender.fillna('Male')
+
+
+# In[9]:
+
+
+data['Married'].value_counts()
+
+
+# In[10]:
+
+
+data.Married = data.Married.fillna('Yes')
+data['Dependents'].value_counts()
+
+
+# In[11]:
+
+
+data.Dependents = data.Dependents.fillna('0')
+
+
+# In[12]:
+
+
+data['Self_Employed'].value_counts()
+
+
+# In[13]:
+
+
+data.Self_Employed = data.Self_Employed.fillna('No')
+data.LoanAmount = data.LoanAmount.fillna(data.LoanAmount.mean())
+data['Loan_Amount_Term'].value_counts()
+
+
+# In[14]:
+
+
+data.Loan_Amount_Term = data.Loan_Amount_Term.fillna(360.0)
+data['Credit_History'].value_counts()
+
+
+# In[15]:
+
+
+data.Credit_History = data.Credit_History.fillna(1.0)
+data.apply(lambda x: sum(x.isnull()),axis=0)
+
+
+# In[16]:
+
+
+data.head()
+
+
+# In[17]:
+
+
+# Splitting traing data
+X = data.iloc[:, 1: 12].values
+y = data.iloc[:, 12].values
+X
+
+
+# In[18]:
+
+
+y
+
+
+# In[19]:
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+X_train
+
+
+# In[20]:
+
+
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.preprocessing import LabelEncoder
+labelencoder_X = LabelEncoder()
+for i in range(0, 5):
+    X_train[:,i] = labelencoder_X.fit_transform(X_train[:,i])
+
+X_train[:,10] = labelencoder_X.fit_transform(X_train[:,10])
+# Encoding the Dependent Variable
+labelencoder_y = LabelEncoder()
+y_train = labelencoder_y.fit_transform(y_train)
+X_train
+
+
+# In[21]:
+
+
+y_train
+
+
+# In[22]:
+
+
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+for i in range(0, 5):
+    X_test[:,i] = labelencoder_X.fit_transform(X_test[:,i])
+X_test[:,10] = labelencoder_X.fit_transform(X_test[:,10])
+# Encoding the Dependent Variable
+labelencoder_y = LabelEncoder()
+y_test = labelencoder_y.fit_transform(y_test)
+X_test
+
+
+# In[23]:
+
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.fit_transform(X_test)
+
+
+# In[24]:
+
+
+# Applying PCA
+from sklearn.decomposition import PCA
+pca = PCA(n_components = 2)
+X_train = pca.fit_transform(X_train)
+X_test = pca.fit_transform(X_test)
+explained_variance = pca.explained_variance_ratio_
+
+
+# In[25]:
+
+
+# Fitting Logistic Regression to the Training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
+
+# In[26]:
+
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+
+# In[27]:
+
+
+y_pred
+
+
+# In[28]:
+
+
+# Measuring Accuracy
+from sklearn import metrics
+print('The accuracy of Logistic Regression is: ', metrics.accuracy_score(y_pred, y_test))
+
+
+# In[29]:
+
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+cm
+
+
+# In[30]:
+
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('pink', 'lightgreen')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+plt.show()
+
+
+# In[31]:
+
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('pink', 'lightgreen')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+plt.show()
+
+
+# In[32]:
+
+
+# Fitting K-NN to the Training set
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
+classifier.fit(X_train, y_train)
+
+
+# In[33]:
+
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred
+
+
+# In[34]:
+
+
+# Measuring Accuracy
+from sklearn import metrics
+print('The accuracy of KNN is: ', metrics.accuracy_score(y_pred, y_test))
+
+
+# In[35]:
+
+
+# Making confusion matrix
+from sklearn.metrics import confusion_matrix
+print(confusion_matrix(y_test, y_pred))
+
+
+# In[36]:
+
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('pink', 'lightgreen')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('KNN (Training set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+plt.show()
+
+
+# In[37]:
+
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('pink', 'lightgreen')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('KNN (Training set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+plt.show()
+
+
+# In[38]:
+
+
+# Fitting SVM to the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'linear', random_state = 0)
+classifier.fit(X_train, y_train)
+
+
+# In[39]:
+
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred
+
+
+# In[40]:
+
+
+# Measuring Accuracy
+from sklearn import metrics
+print('The accuracy of SVM is: ', metrics.accuracy_score(y_pred, y_test))
+
+
+# In[41]:
+
+
+# Making confusion matrix
+from sklearn.metrics import confusion_matrix
+print(confusion_matrix(y_test, y_pred))
+
+
+# In[42]:
+
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('pink', 'lightgreen')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('SVM (Training set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+plt.show()
+
+
+# In[44]:
+
+
+#Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                    np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+            alpha = 0.75, cmap = ListedColormap(('pink', 'lightgreen')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+   plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+               c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('SVM (Test set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+plt.show()
+
+
+# In[45]:
+
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+
+# In[46]:
+
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred
+
+
+# In[47]:
+
+
+# Measuring Accuracy
+from sklearn import metrics
+print('The accuracy of Naive Bayes is: ', metrics.accuracy_score(y_pred, y_test))
+
+
+# In[48]:
+
+
+# Making confusion matrix
+from sklearn.metrics import confusion_matrix
+print(confusion_matrix(y_test, y_pred))
+
+
+# In[49]:
+
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('pink', 'lightgreen')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Naive Bayes (Training set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+plt.show()
+
+
+# In[50]:
+
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('pink', 'lightgreen')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Naive Bayes (Test set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+plt.show()
+
+
+# In[51]:
+
+
+# Fitting Decision Tree Classification to the Training set
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
+classifier.fit(X_train, y_train)
+
+
+# In[52]:
+
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred
+
+
+# In[53]:
+
+
+# Measuring Accuracy
+from sklearn import metrics
+print('The accuracy of Decision Tree Classifier is: ', metrics.accuracy_score(y_pred, y_test))
+
+
+# In[54]:
+
+
+# Making confusion matrix
+from sklearn.metrics import confusion_matrix
+print(confusion_matrix(y_test, y_pred))
+
+
+# In[55]:
+
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('pink', 'lightgreen')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Decision Tree Classifier (Training set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+plt.show()
+
+
+# In[56]:
+
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('pink', 'lightgreen')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Decision Tree Classifier (Test set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+plt.show()
+
+
+# In[57]:
+
+
+# Fitting Random Forest Classification to the Training set
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
+classifier.fit(X_train, y_train)
+
+
+# In[58]:
+
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred
+
+
+# In[59]:
+
+
+# Measuring Accuracy
+from sklearn import metrics
+print('The accuracy of Random Forest Classification is: ', metrics.accuracy_score(y_pred, y_test))
+
+
+# In[60]:
+
+
+# Making confusion matrix
+from sklearn.metrics import confusion_matrix
+print(confusion_matrix(y_test, y_pred))
+
+
+# In[61]:
+
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('pink', 'lightgreen')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Random Forest Classification (Training set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+plt.show()
+
+
+# In[62]:
+
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('pink', 'lightgreen')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Random Forest Classification (Test set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+plt.show()
+
+
+# Results:
+# The accuracy of Logistic Regression is: 70.73 %
+# 
+# The accuracy of KNN is: 62.92 %
+# 
+# The accuracy of SVM is: 70.73 %
+# 
+# The accuracy of Naive Bayes is: 71.21 %
+# 
+# The accuracy of Decision Tree Classifier is: 53.63 %
+# 
+# The accuracy of Random Forest Classification is: 58.53 %
+
+# In[ ]:
+
+
+
+
